2aa446c3eace1af5d1a86eec577208552fad6635
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-03-12 15:07:25 +0100

    finish

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const currentScore = document.getElementsByClassName("current");
 
 const rollDice = document.getElementById("rollDice");
 const resultOfDice = document.getElementById("result");
-const hold = document.getElementById('hold');
+const hold = document.getElementById("hold");
 
-const circlePlayerActive = document.getElementsByClassName('fa-circle');
+const circlePlayerActive = document.getElementsByClassName("fa-circle");
 
-const gameBoardPlayerOne = document.getElementsByClassName('playerOne');
-const gameBoardPlayerTwo = document.getElementsByClassName('playerTwo');
+const gameBoardPlayerOne = document.getElementsByClassName("playerOne");
+const gameBoardPlayerTwo = document.getElementsByClassName("playerTwo");
 
 let arrayScore = [];
 let activePlayer = 0;
-
+let resultOfDiceRoll = 2;
 
 newGame.addEventListener("click", startNewGame);
 rollDice.addEventListener("click", rollDiceGame);
-hold.addEventListener('click', holdScore);
-
+hold.addEventListener("click", holdScore);
 
 function startNewGame() {
   for (element of globalScore) {
@@ -28,16 +27,15 @@ function startNewGame() {
     element.firstChild.textContent = 0;
   }
   arrayScore = [];
-  gameActiveDisplay()
+  gameActiveDisplay();
 }
 
 function rollDiceGame() {
-  resultOfDice.innerHTML = Math.floor(Math.random() * 6 + 1);
-  arrayScore.push(Number(resultOfDice.innerHTML));
-  
+  resultOfDiceRoll = Math.floor(Math.random() * 6 + 1);
+  arrayScore.push(resultOfDiceRoll);
   let totalCurrentScore = 0;
-  
-  if (resultOfDice.innerHTML == 1) {
+
+  if (resultOfDiceRoll == 1) {
     currentScore[activePlayer].innerHTML = 0;
     arrayScore = [];
     switchPlayer();
@@ -47,6 +45,7 @@ function rollDiceGame() {
       currentScore[activePlayer].innerHTML = totalCurrentScore;
     }
   }
+  getDiceImg()
 }
 
 function holdScore() {
@@ -54,39 +53,43 @@ function holdScore() {
   globalNumber += Number(currentScore[activePlayer].innerHTML);
   globalScore[activePlayer].innerHTML = globalNumber;
   // currentScore[activePlayer].innerHTML = 0;
-  arrayScore= []
-  
+  arrayScore = [];
+
   gameWin();
   switchPlayer();
 }
 
-function switchPlayer() { 
+function switchPlayer() {
   if (activePlayer === 0) {
-    activePlayer = 1
+    activePlayer = 1;
   } else {
-    activePlayer = 0
+    activePlayer = 0;
   }
-  gameActiveDisplay()
+  gameActiveDisplay();
 }
 
 function gameActiveDisplay() {
-  if( activePlayer === 0) {
-    circlePlayerActive[0].style.visibility = 'visible';
-    circlePlayerActive[1].style.visibility = 'hidden';
+  if (activePlayer === 0) {
+    circlePlayerActive[0].style.visibility = "visible";
+    circlePlayerActive[1].style.visibility = "hidden";
     gameBoardPlayerOne[0].classList.add("bgColor");
     gameBoardPlayerTwo[0].classList.remove("bgColor");
   } else {
-    circlePlayerActive[0].style.visibility = 'hidden';
-    circlePlayerActive[1].style.visibility = 'visible';
+    circlePlayerActive[0].style.visibility = "hidden";
+    circlePlayerActive[1].style.visibility = "visible";
     gameBoardPlayerOne[0].classList.remove("bgColor");
     gameBoardPlayerTwo[0].classList.add("bgColor");
   }
 }
 
 function gameWin() {
-  if (globalScore[activePlayer].innerHTML >= 100){
+  if (globalScore[activePlayer].innerHTML >= 100) {
     // setTimeout(() => {
-      //   alert(`Player ${activePlayer +1}, Vous avez gagné!`)},1000);
-        alert (`Player ${activePlayer +1}, vous avez gagné!`)
-      }
-    }
+    //   alert(`Player ${activePlayer +1}, Vous avez gagné!`)},1000);
+    alert(`Player ${activePlayer + 1}, vous avez gagné!`);
+  }
+}
+
+function getDiceImg() {
+  resultOfDice.setAttribute("src", `/images/dice${resultOfDiceRoll}.png`);
+}
